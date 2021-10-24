a304d757f7d532c54a177ac8b3c4a6885777d7e1
Oleg Romanov <55031494+oleg-romanov@users.noreply.github.com>
2021-10-24 17:39:15 +0300

    added xcode project

---
+++ AdvancedUI/AdvancedUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedUI
+//
+//  Created by Олег Романов on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
